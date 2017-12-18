2c0d5959a7f84170fa830337a7d30c10fff716b7
Neil Ni <neilnee324@gmail.com>
2017-12-18 12:56:44 +0800

    增加一些测试代码

--- catchme/catchme/__test__.py
+++ catchme/catchme/__test__.py
@@ -1,11 +1,23 @@
 # coding=utf-8
 
-from openpyxl import load_workbook
+import json
+import urllib2
 
-wb = load_workbook('revenue/catchme_ch/revenue_2017-12-09_ch.xlsx')
-
-ws = wb.get_sheet_by_name(wb.get_sheet_names()[0])
-
-for row in ws.rows:
-    print(row[3].value)
+with open("ch_img.json") as img_file:
+    load_ch = json.load(img_file)
+    count = 0
+    url_all = []
+    for i in range(1, 139):
+        # url_list = load_ch['machine/qr_code/CATCH_' + str(100000 + i) + '.png']
+        url_list = load_ch[str(i)]
+        for url in url_list:
+            req = urllib2.Request(url)
+            # noinspection PyBroadException
+            try:
+                fd = urllib2.urlopen(req, timeout=0.1)
+                print('request finish ' + str(100000 + i) + 'url ' + url)
+            except Exception:
+                print('request exception ' + str(100000 + i) + ' url ' + url)
+            count += 1
+    print("total count : " + str(count))
 
